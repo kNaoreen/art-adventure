f587e46e5983642d93068b2cf9b7d298bb765f74
Naoreen K <naowereen@gmail.com>
2022-11-21 19:47:00 -0800

    setup home page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <h1>Art Adventure</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          how would you like to explore art today?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>make a gallery</button>
       </header>
     </div>
   );
