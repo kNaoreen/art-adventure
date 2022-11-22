726b48bb2294d830b1fd193ec8998a724aeedf46
Naoreen K <naowereen@gmail.com>
2022-11-21 20:48:04 -0800

    routing is working but css is not =(

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
+import GalleryMaker from "./make-a-gallery/galleryMaker";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <h1>Art Adventure</h1>
-        <p>
-          how would you like to explore art today?
-        </p>
-        <button>make a gallery</button>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="make-a-gallery/" element={<GalleryMaker />} />
+          </Routes>
+      </BrowserRouter>
   );
+}
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                <h1>Art Adventure</h1>
+                <p>
+                    how would you like to explore art today?
+                </p>
+                {/*TODO: I really wish this was an actual button! :(*/}
+                <Link className="optionButton" to="make-a-gallery">make a gallery</Link>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/make-a-gallery/galleryMaker.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GalleryMaker = (props) => {
+    return (
+        <div>
+            <h1>
+                Make A Gallery
+            </h1>
+            <p> This is a work in progress </p>
+        </div>
+    );
+};
+
+export default GalleryMaker;
